601c95d6d82d9b6571d1b0ea1cb464644e10c45e
CRTYPUBG <a95585995@gmail.com>
2025-06-13 13:59:05 +0300

    Add files via upload

---
+++ Key.js
@@ -0,0 +1,5 @@
+const KeyAuthApp = new KeyAuth({
+    name: "game-opti", // App name 
+    ownerid: "eyiFqt1JTu", // Account ID
+    version: "1.0", // Application version. Used for automatic downloads see video here https://www.youtube.com/watch?v=kW195PLCBKs
+})
